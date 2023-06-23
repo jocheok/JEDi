2d272647147752eb5486d923455b2f963963a216
josephcheok <joseph@cheokandco.com>
2023-06-23 23:03:10 +1000

    Import initalize app on firebaseInit.js

--- assets/js/firebaseInit.js
+++ assets/js/firebaseInit.js
@@ -1,10 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyCnDefhLLKDJv5pUBr7FN4-FGASb13-3m0",
     authDomain: "project-2-4051e.firebaseapp.com",
     databaseURL: "https://project-2-4051e.firebaseio.com",
     projectId: "project-2-4051e",
-    storageBucket: "",
+    storageBucket: "project-2-4051e.appspot.com",
     messagingSenderId: "161199308928",
     appId: "1:161199308928:web:29c3bb574806a01f"
 };
